55f2340b75ef97c983a41a0f88171664325832bc
Stewkowski <stevesa23@gmail.com>
2020-03-22 12:51:12 -0500

    sql 4

--- queries.sql
+++ queries.sql
@@ -14,3 +14,9 @@ WHERE (attending = "St. Louis City")
 DROP TABLE job
 
 ## Part 4: Test it with SQL
+
+SELECT name, description
+FROM skill ORDER BY name
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+INNER JOIN job ON job_skills.job_id = job.id
+WHERE job_id IS NOT NULL
